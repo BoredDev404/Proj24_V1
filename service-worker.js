5002b8428f6120f2e0b403f296aaf042d89c1cd8
BoredDev404 <rihazrizvi@gmail.com>
2025-12-20 03:22:44 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+// service-worker.js
+const CACHE_NAME = 'life-tracker-v24.1';
+const urlsToCache = [
+  '/Proj24_V1/',
+  '/Proj24_V1/index.html',
+  '/Proj24_V1/style.css',
+  '/Proj24_V1/app.js',
+  '/Proj24_V1/db.js',
+  '/Proj24_V1/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/dexie@3.2.4/dist/dexie.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
